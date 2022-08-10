7346390cfad6ec7e6934bef62ecfeff8dd9aa89d
JaeWooYi <wodn0101@gmail.com>
2022-08-10 18:54:41 +0900

    수요일

--- SpringMVC01/src/main/java/kr/board/controller/BoardController.java
+++ SpringMVC01/src/main/java/kr/board/controller/BoardController.java
@@ -1,8 +1,19 @@
 package kr.board.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BoardController {
 
+	// /boardList.do라는 요청이 오면?
+	// HandlerMapping
+	@RequestMapping("/boardList.do")
+	public String boardList() {
+		// retrun타입이 view페이지의 경로니까 String
+		
+		return "boardList";
+	}
+	
+	
 }
