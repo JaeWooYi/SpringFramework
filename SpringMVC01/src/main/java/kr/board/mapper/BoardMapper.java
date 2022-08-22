3ff7d57f25a9e1818dae9deb993d0e49cd0d20c7
JaeWooYi <wodn0101@gmail.com>
2022-08-22 15:29:12 +0900

    Monday

--- SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
+++ SpringMVC01/src/main/java/kr/board/mapper/BoardMapper.java
@@ -2,10 +2,12 @@ package kr.board.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import kr.board.entity.Board;
 
 // mybatis-api를 쓰면 아래 어노 테이션을 쓸 수 있다.
-//@Mapper
+@Mapper
 public interface BoardMapper {
 	
 	public List<Board> getLists();	// 게시판 전체 리스트
