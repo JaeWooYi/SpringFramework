997a41dd405c611a3db0e3aa4dee5d3df3bd88d8
JaeWooYi <wodn0101@gmail.com>
2022-08-28 11:33:00 +0900

    Sunday....

--- SpringMVC01/src/main/java/kr/board/entity/board.sql
+++ SpringMVC01/src/main/java/kr/board/entity/board.sql
@@ -1,2 +1,15 @@
 
 
+create table myboard(
+idx int not null auto_increment,
+title varchar(100) not null,
+content varchar(2000) not null,
+writer varchar(30) not null,
+indate datetime default now(),
+count int default 0,
+primary key(idx)
+);
+
+insert into myboard (title, content, writer) values ('Test Board', 'Practice board', 'admin');
+insert into myboard (title, content, writer) values ('Test Board', 'Practice board', 'tester');
+insert into myboard (title, content, writer) values ('Test Board', 'Practice board', 'KKK');
